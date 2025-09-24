797231109c4eeb81a7d52962a1ccf3aee6ac24ac
kaji <k.notsukitaira.sys23@morijyobi.ac.jp>
2025-09-24 15:03:20 +0900

    コンポーネントの学習１

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>Hello World!!!!</p>
+      <p>こんにちはReact!!!!</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Mycomponent.jsx
@@ -0,0 +1,5 @@
+//export defaultを設定するとこのファイルに複数関数があった場合でも
+//これを設定した関数が代表として出力される
+export default function MyComponent() {
+  return <p>これはMyComponentから出力している文字列です</p>;
+}
